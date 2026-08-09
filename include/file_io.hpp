f5ea4b36f18bb68e3a4e8f16c1faf85922b50faf
zcarbona <alialmohallawy310r@gmail.com>
2026-08-09 15:33:18 +0300

    functon declaration

---
+++ include/file_io.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include<cstdint>
+#include <vector>
+#include <filesystem>
+#include <string>
+
+
+namespace file_io
+{
+
+std::vector<std::uint8_t> read_file(const std::filesystem::path& path);
+
+bool write_file(const std::filesystem::path& path, const std::vector<std::uint8_t>& data);
+
+std::uint64_t get_file_size(const std::filesystem::path& path);
+
+
+bool file_exists(const std::filesystem::path& path);
+}
